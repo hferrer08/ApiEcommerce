fe5ed032a179ad843c3ce032b3b4e732e75b5357
hferrer08 <hubert.ferrerg8@gmail.com>
2025-08-19 21:58:04 -0400

    Se crea primer endpoint para listado de categories, utilizando patron repository y automapper

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConnectionString));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+//builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(cfg => {
+
+    // cfg.AddProfile<CategoryProfile>();
+
+    cfg.AddMaps(typeof(Program).Assembly);
+
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
