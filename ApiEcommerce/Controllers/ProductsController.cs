466a3c2522342d493b5aa3d0ba7520f6f69d977b
hferrer08 <hubert.ferrerg8@gmail.com>
2025-08-25 22:45:55 -0400

    Se crea endpoint para listar products

--- ApiEcommerce/Controllers/ProductsController.cs
+++ ApiEcommerce/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -9,12 +10,24 @@ namespace ApiEcommerce.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-         private readonly IProductRepository _productRepository;
+        private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
         public ProductsController(IProductRepository productRepository, IMapper mapper)
         {
             _productRepository = productRepository;
             _mapper = mapper;
         }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(List<ProductDTO>), StatusCodes.Status200OK)]
+
+        public IActionResult GetCategories()
+        {
+            var products = _productRepository.GetProducts();
+            var productsDto = _mapper.Map<List<ProductDTO>>(products);
+            return Ok(productsDto);
+        }
+
     }
 }
