44fab1c9ac9a5431ffeea79e2dee2b733c69a6bc
hferrer08 <hubert.ferrerg8@gmail.com>
2025-08-25 22:34:41 -0400

    Se crea controlador para Products

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -8,6 +8,7 @@ var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql"
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConnectionString));
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 //builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddAutoMapper(cfg => {
 
