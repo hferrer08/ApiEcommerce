d1583783b0c9b15ca1dbad411ef435ca7db888d4
hferrer08 <hubert.ferrerg8@gmail.com>
2025-09-10 22:04:52 -0300

    Se realizan prueba de los endpoint con swagger y se realizan correcciones, queda ok

--- ApiEcommerce/Models/Dtos/UserDto.cs
+++ ApiEcommerce/Models/Dtos/UserDto.cs
@@ -4,10 +4,9 @@ namespace ApiEcommerce.Models.Dtos;
 
 public class UserDto
 {
-    public int Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
     public string? Name { get; set; }
     public string? Username { get; set; }
-    public string? Password { get; set; }
-    public string? Role { get; set; }
+  
 }
