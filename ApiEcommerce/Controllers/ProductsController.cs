3c2b7e744b090ce408ec265fce68d6cebddb5389
hferrer08 <hubert.ferrerg8@gmail.com>
2025-09-12 21:30:26 -0300

    Se crea endpoint para paginacion

--- ApiEcommerce/Controllers/ProductsController.cs
+++ ApiEcommerce/Controllers/ProductsController.cs
@@ -28,7 +28,7 @@ namespace ApiEcommerce.Controllers
         }
 
         [HttpGet]
-         [AllowAnonymous]
+        [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(List<ProductDTO>), StatusCodes.Status200OK)]
 
@@ -57,6 +57,38 @@ namespace ApiEcommerce.Controllers
             var productDto = _mapper.Map<ProductDTO>(product);
 
             return Ok(productDto);
+        }
+
+        [HttpGet("Paged", Name = "GetProductsInPage")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+
+        public IActionResult GetProductsInPage([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 5)
+        {
+            if (pageNumber < 1 || pageSize < 1)
+            {
+                return BadRequest("Los parámetros de paginación no son válidos.");
+            }
+            var totalProducts = _productRepository.GetTotalProducts();
+            var totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+            if (pageNumber > totalPages)
+            {
+                return NotFound("No hay más páginas disponibles");
+            }
+            var products = _productRepository.GetProductsInPages(pageNumber, pageSize);
+            var productsDto = _mapper.Map<List<ProductDTO>>(products);
+            var paginationResponse = new
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalPages = totalPages,
+                Products = productsDto
+            };
+            return Ok(paginationResponse);
         }
 
         [HttpPost]
